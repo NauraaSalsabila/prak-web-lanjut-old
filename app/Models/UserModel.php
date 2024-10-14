5a28e5683bf7628b10699d02232f60211bfc7c7f
NauraaSalsabila <nauras294@gmail.com>
2024-10-14 13:23:02 +0700

    modul 7

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -24,10 +24,18 @@ class UserModel extends Model
         return $this->belongsTo(Kelas::class, 'kelas_id');
     }
 
-    public function getUser()
-    {
+    public function getUser($id = null) {
+        if ($id != null) {
+            return $this->join('kelas', 'kelas.id', '=', 'user.kelas_id')
+                ->select('user.*', 'kelas.nama_kelas')
+                ->where('user.id', $id)
+                ->first();
+        }
+    
         return $this->join('kelas', 'kelas.id', '=', 'user.kelas_id')
-                    ->select('user.*', 'kelas.nama_kelas as nama_kelas')
-                    ->get();
+            ->select('user.*', 'kelas.nama_kelas')
+            ->orderBy('user.id', 'asc')
+            ->get();
     }
+    
 }
--- resources/views/list_user.blade.php
+++ resources/views/list_user.blade.php
@@ -15,6 +15,7 @@
                 <th>Nama</th>
                 <th>NPM</th>
                 <th>Kelas</th>
+                <th>Foto</th> 
                 <th>Aksi</th>
             </tr>
         </thead>
@@ -25,9 +26,22 @@
                 <td>{{ $user['nama'] }}</td>
                 <td>{{ $user['npm'] }}</td>
                 <td>{{ $user['nama_kelas'] }}</td>
-                <td class="action-buttons">
-                    <button class="btn btn-edit">Edit</button>
-                    <button class="btn btn-delete">Hapus</button>
+                <td>
+                    @if ($user['foto']) 
+                        <img src="{{ asset($user['foto']) }}" alt="Foto Profil" style="width: auto; height: 100px; object-fit: cover; border: 1px solid #ccc; border-radius: 5px;">
+                    @else
+                        <span>Tidak ada foto</span>
+                    @endif
+                </td>
+                <td class="text-center">
+                    <div style="display: flex; justify-content: center;"> 
+                        <button type="button" class="btn action-btn view mx-1" onclick="window.location.href='{{ route('user.show', $user['id']) }}'">View</button>
+                        <button type="button" class="btn action-btn edit mx-1" onclick="window.location.href='{{ route('user.edit', $user['id']) }}'">Edit</button>
+                        <form action="{{ route('user.destroy', $user['id']) }}" method="POST" class="delete-form" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn action-btn mx-1" onclick="return confirm('Apakah Anda yakin ingin menghapus pengguna ini?')">Delete</button>
+                        </form>
                 </td>
             </tr>
         @endforeach
